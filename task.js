08897d0a596ae3b49c4fb2bc72d26106d2735a39
ImTariquL <imtariqul21@gmail.com>
2025-05-18 23:08:41 +0600

    small vs big

---
+++ task.js
@@ -0,0 +1,25 @@
+
+// Task 1
+
+let totalMoney = 1000;
+let orange = 300;
+let apples = 400;
+let costMoney = orange + apples;
+let remainMoney = totalMoney - costMoney;
+
+console.log(remainMoney);
+
+
+
+// Task 1
+
+var burger = 400 ;
+
+if(burger > 500){
+    console.log ('your coke with burger is free');
+}
+else{
+    console.log('your coke coke with burger is 30 tk');
+}
+
+
